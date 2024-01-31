644ce0d953c0429b7ab1ad78fda2175351c864cd
EVELYNKINYA <evelynkinya156@gmail.com>
2024-01-31 14:38:03 +0300

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,16 +16,37 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+/* write an <About> component here */
+function About (){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
+      {/* add the <Home> component here */
+      function Home() {
+        return (
+          <div id="home">
+            <h1>Home</h1>
+          </div>
+        );
+      }
+      }
+      {
+      function About (){
+        return (
+          <div id="about">
+            <h2>About</h2>
+          </div>
+        );
+      }}
+        </div>
   );
 }
 
